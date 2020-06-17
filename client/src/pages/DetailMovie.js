01a504f6275b70a285e499a968803d801c6136b0
crusherblack <fadhildarma13@gmail.com>
2020-06-17 20:20:23 +0700

    fix fetch data

--- client/src/pages/DetailMovie.js
+++ client/src/pages/DetailMovie.js
@@ -1,85 +1,95 @@
-import React, { useEffect } from "react";
-import { useHistory } from "react-router-dom";
-import { connect } from "react-redux";
-import { useDispatch, useSelector } from "react-redux";
-import { getDetailsFilm } from "../redux/actions/film";
-import PropTypes from "prop-types";
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { getDetailsFilm } from '../redux/actions/film';
+import PropTypes from 'prop-types';
 
-import VideoThumbnail from "../components/VideoThumbnail/VideoThumbnail";
-import Description from "../components/MovieDetail/Description";
-import Episode from "../components/MovieDetail/Episode";
-import Movie from "../components/MovieDetail/Movie";
-import "./css/DetailMovie.css";
+import VideoThumbnail from '../components/VideoThumbnail/VideoThumbnail';
+import Description from '../components/MovieDetail/Description';
+import Episode from '../components/MovieDetail/Episode';
+import Movie from '../components/MovieDetail/Movie';
+import './css/DetailMovie.css';
 
-import moviesThumbnail from "../img/videothumbnail/video2.png";
-import tvThumbnail from "../img/videothumbnail/video1.png";
+import moviesThumbnail from '../img/videothumbnail/video2.png';
+import tvThumbnail from '../img/videothumbnail/video1.png';
 
-const DetailMovie = ({
-  getDetailsFilm,
-  match,
-  film: { filmDetails, loading },
-}) => {
-  useEffect(() => {
-    getDetailsFilm(match.params.id);
-  }, [getDetailsFilm]);
+const DetailMovie = ({ getDetailsFilm, match, film: { filmDetails, loading } }) => {
+	useEffect(
+		() => {
+			getDetailsFilm(match.params.id);
+		},
+		[ getDetailsFilm ]
+	);
 
-  let history = useHistory();
-  let role = "admin";
+	let history = useHistory();
+	let role = 'admin';
 
-  const openAddEpisode = () => {
-    history.push("/add-episode");
-  };
+	const openAddEpisode = () => {
+		history.push('/add-episode');
+	};
 
-  const { category } = filmDetails;
-
-  return loading ? (
-    <div style={{ top: "500px" }}>rendering...</div>
-  ) : (
-    <div style={{ marginTop: "70px" }}>
-      <VideoThumbnail
-        thumbnail={category.id == 1 ? tvThumbnail : moviesThumbnail}
-      />
-      {role == "admin" && category.id == 1 ? (
-        <div
-          style={{
-            height: "50px",
-            position: "relative",
-          }}
-        >
-          <button
-            className="btn-merah"
-            style={{
-              float: "right",
-              width: "12em",
-              top: "1em",
-              right: "2em",
-              marginBottom: "3em",
-            }}
-            onClick={() => openAddEpisode()}
-          >
-            Add Episode
-          </button>
-        </div>
-      ) : null}
-      <div className="details">
-        <Description filmDetails={filmDetails} />
-        {category.id == 1 ? (
-          <Episode name={filmDetails.title} />
-        ) : (
-          <Movie name={filmDetails.title} />
-        )}
-      </div>
-    </div>
-  );
+	return (
+		<>
+			{filmDetails === null || loading ? (
+				<>
+					<p>Loading</p>
+				</>
+			) : (
+				<>
+					<div style={{ marginTop: '70px' }}>
+						<VideoThumbnail
+							thumbnail={
+								filmDetails.category.id == 1 ? (
+									tvThumbnail
+								) : (
+									moviesThumbnail
+								)
+							}
+						/>
+						{role == 'admin' && filmDetails.category.id == 1 ? (
+							<div
+								style={{
+									height: '50px',
+									position: 'relative'
+								}}
+							>
+								<button
+									className="btn-merah"
+									style={{
+										float: 'right',
+										width: '12em',
+										top: '1em',
+										right: '2em',
+										marginBottom: '3em'
+									}}
+									onClick={() => openAddEpisode()}
+								>
+									Add Episode
+								</button>
+							</div>
+						) : null}
+						<div className="details">
+							<Description filmDetails={filmDetails} />
+							{filmDetails.category.id == 1 ? (
+								<Episode name={filmDetails.title} />
+							) : (
+								<Movie name={filmDetails.title} />
+							)}
+						</div>
+					</div>
+				</>
+			)}
+		</>
+	);
 };
 
 DetailMovie.propTypes = {
-  getDetailsFilm: PropTypes.func.isRequired,
-  film: PropTypes.object.isRequired,
+	getDetailsFilm: PropTypes.func.isRequired,
+	film: PropTypes.object.isRequired
 };
 
 const mapStateToProps = (state) => ({
-  film: state.film,
+	film: state.film
 });
 
 export default connect(mapStateToProps, { getDetailsFilm })(DetailMovie);
--- client/src/redux/actions/film.js
+++ client/src/redux/actions/film.js
@@ -1,93 +1,73 @@
-import axios from "axios";
+import axios from 'axios';
 
 import {
-  GET_FILMS_ALL,
-  GET_FILMS_MOVIES,
-  GET_FILMS_TVSERIES,
-  GET_FILMS_DETAILS,
-  LOADING_TRUE,
-  ERROR,
-} from "./types";
+	GET_FILMS_ALL,
+	GET_FILMS_MOVIES,
+	GET_FILMS_TVSERIES,
+	GET_FILMS_DETAILS,
+	LOADING_TRUE,
+	ERROR
+} from './types';
 
 //Get All Films
 export const getFilmsAll = (limit) => async (dispatch) => {
-  dispatch({
-    type: LOADING_TRUE,
-  });
-  try {
-    let res = await axios.get(`/api/v1/film?page=1&limit=${limit}`);
-
-    dispatch({
-      type: GET_FILMS_ALL,
-      payload: res.data.data,
-    });
-  } catch (err) {
-    dispatch({
-      type: ERROR,
-      payload: "error",
-    });
-  }
+	try {
+		let res = await axios.get(`/api/v1/film?page=1&limit=${limit}`);
+		dispatch({
+			type: GET_FILMS_ALL,
+			payload: res.data.data
+		});
+	} catch (err) {
+		dispatch({
+			type: ERROR,
+			payload: 'error'
+		});
+	}
 };
 
 //Get Films Movies
 export const getFilmsMovies = (limit) => async (dispatch) => {
-  dispatch({
-    type: LOADING_TRUE,
-  });
-  try {
-    let res = await axios.get(
-      `/api/v1/film?page=1&limit=${limit}&categoryId=2`
-    );
-
-    dispatch({
-      type: GET_FILMS_MOVIES,
-      payload: res.data.data,
-    });
-  } catch (err) {
-    dispatch({
-      type: ERROR,
-      payload: "error",
-    });
-  }
+	try {
+		let res = await axios.get(`/api/v1/film?page=1&limit=${limit}&categoryId=2`);
+		dispatch({
+			type: GET_FILMS_MOVIES,
+			payload: res.data.data
+		});
+	} catch (err) {
+		dispatch({
+			type: ERROR,
+			payload: 'error'
+		});
+	}
 };
 
 //Get Films TVSeries
 export const getFilmsTVSeries = (limit) => async (dispatch) => {
-  dispatch({
-    type: LOADING_TRUE,
-  });
-  try {
-    let res = await axios.get(
-      `/api/v1/film?page=1&limit=${limit}&categoryId=1`
-    );
-
-    dispatch({
-      type: GET_FILMS_TVSERIES,
-      payload: res.data.data,
-    });
-  } catch (err) {
-    dispatch({
-      type: ERROR,
-      payload: "error",
-    });
-  }
+	try {
+		let res = await axios.get(`/api/v1/film?page=1&limit=${limit}&categoryId=1`);
+		dispatch({
+			type: GET_FILMS_TVSERIES,
+			payload: res.data.data
+		});
+	} catch (err) {
+		dispatch({
+			type: ERROR,
+			payload: 'error'
+		});
+	}
 };
 
 export const getDetailsFilm = (id) => async (dispatch) => {
-  dispatch({
-    type: LOADING_TRUE,
-  });
-  try {
-    let res = await axios.get(`/api/v1/film/${id}`);
-
-    dispatch({
-      type: GET_FILMS_DETAILS,
-      payload: res.data.data,
-    });
-  } catch (err) {
-    dispatch({
-      type: ERROR,
-      payload: "error",
-    });
-  }
+	try {
+		let res = await axios.get(`/api/v1/film/${id}`);
+		dispatch({
+			type: GET_FILMS_DETAILS,
+			payload: res.data.data
+		});
+	} catch (err) {
+		dispatch({
+			type: ERROR,
+			payload: 'error'
+		});
+	}
 };
