782876198adc71f44405da9c5776bb6a8b888633
crusherblack <fadhildarma13@gmail.com>
2020-06-22 00:19:34 +0700

    transcation

--- client/src/pages/IncomingTransaction.js
+++ client/src/pages/IncomingTransaction.js
@@ -6,12 +6,16 @@ import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
 import { connect } from 'react-redux';
 import { getTransaction, updatePayment } from '../redux/actions/payment';
 import PropTypes from 'prop-types';
+import dayjs from 'dayjs';
 
 const IncomingTransaction = ({
 	getTransaction,
 	updatePayment,
 	payment: { transaction, loading }
 }) => {
+	let dateNow = dayjs();
+	let now = dateNow.format('YYYY-MM-DD');
+
 	useEffect(
 		() => {
 			getTransaction();
@@ -23,80 +27,85 @@ const IncomingTransaction = ({
 		updatePayment(status, idTransaction, idUser);
 	};
 
-	const data = transaction.map((data, index) => (
-		<tr key={data.id}>
-			<td>{index + 1}</td>
-			<td>{data.userInfo.fullName}</td>
-			<td>
-				<img
-					style={{ width: '200px' }}
-					src={`http://localhost:5000/uploads/${data.attache}`}
-					alt="data"
-				/>
-			</td>
-			<td>{data.userInfo.dueDate}</td>
-			<td
-				style={{
-					color: data.userInfo.subscribe ? '#0ACF83' : '#FF0742'
-				}}
-			>
-				{data.userInfo.subscribe ? 'Active' : 'Not Active'}
-			</td>
-			<td
-				style={{
-					color:
-						data.status == 'Approved'
-							? '#0ACF83'
-							: data.status == 'Pending' ? '#F7941E' : '#FF0742'
-				}}
-			>
-				{data.status}
-			</td>
-			<td>
-				<span
+	const data = transaction.map((data, index) => {
+		let due_Date = dayjs(data.userInfo.dueDate);
+		return (
+			<tr key={data.id}>
+				<td>{index + 1}</td>
+				<td>{data.userInfo.fullName}</td>
+				<td>
+					<img
+						style={{ width: '200px' }}
+						src={`http://localhost:5000/uploads/${data.attache}`}
+						alt="data"
+					/>
+				</td>
+				<td>
+					{due_Date.diff(now, 'day') > 0 ? due_Date.diff(now, 'day') : 0} Days
+				</td>
+				<td
 					style={{
-						fontSize: '20px',
-						cursor: 'pointer',
-						color: '#1C9CD2'
+						color: data.userInfo.subscribe ? '#0ACF83' : '#FF0742'
 					}}
 				>
-					{data.status === 'Approved' || data.status === 'Reject' ? null : (
-						<div className="dropdown">
-							<div className="dropdown-content">
-								<label htmlFor="">
-									<ul>
-										<li
-											style={{ color: '#0ACF83' }}
-											onClick={() =>
-												handleUpdate(
-													'Approved',
-													data.id,
-													data.userInfo.id
-												)}
-										>
-											Aktifasi
-										</li>
-										<li
-											style={{ color: 'red' }}
-											onClick={() =>
-												handleUpdate(
-													'Reject',
-													data.id,
-													data.userInfo.id
-												)}
-										>
-											Reject
-										</li>
-									</ul>
-								</label>
+					{data.userInfo.subscribe ? 'Active' : 'Not Active'}
+				</td>
+				<td
+					style={{
+						color:
+							data.status == 'Approved'
+								? '#0ACF83'
+								: data.status == 'Pending' ? '#F7941E' : '#FF0742'
+					}}
+				>
+					{data.status}
+				</td>
+				<td>
+					<span
+						style={{
+							fontSize: '20px',
+							cursor: 'pointer',
+							color: '#1C9CD2'
+						}}
+					>
+						{data.status === 'Approved' || data.status === 'Reject' ? null : (
+							<div className="dropdown">
+								<div className="dropdown-content">
+									<label htmlFor="">
+										<ul>
+											<li
+												style={{ color: '#0ACF83' }}
+												onClick={() =>
+													handleUpdate(
+														'Approved',
+														data.id,
+														data.userInfo.id
+													)}
+											>
+												Aktifasi
+											</li>
+											<li
+												style={{ color: 'red' }}
+												onClick={() =>
+													handleUpdate(
+														'Reject',
+														data.id,
+														data.userInfo.id
+													)}
+											>
+												Reject
+											</li>
+										</ul>
+									</label>
+								</div>
+								<FontAwesomeIcon icon={faCaretDown} />
 							</div>
-							<FontAwesomeIcon icon={faCaretDown} />
-						</div>
-					)}
-				</span>
-			</td>
-		</tr>
-	));
+						)}
+					</span>
+				</td>
+			</tr>
+		);
+	});
 
 	return (
 		<div className="transcation-container">
